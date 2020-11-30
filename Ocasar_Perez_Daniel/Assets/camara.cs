14d706057df42d00e41c5335cb690494877ff620
DanielOcasar <daniel.ocasar.perez@gmail.com>
2020-11-30 17:05:15 +0100

    Seguimiento de la camara
    En este commit he hecho que la cámara siga al personaje

---
+++ Ocasar_Perez_Daniel/Assets/camara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y - 5, playerPosition.position.z - 10);
+    }
+}
