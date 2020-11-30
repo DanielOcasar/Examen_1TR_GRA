ce5b521b9356eb504af619612d326ec7440b4893
DanielOcasar <daniel.ocasar.perez@gmail.com>
2020-11-30 16:17:00 +0100

    Interaccion y compatibilidad
    He hecho el primer apartado del examen, que es que la bola se mueva y tenga restingido el movimiento, a priori, no me da fallos, ya iremos viendo jaja...

---
+++ Ocasar_Perez_Daniel/Assets/esfera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class esfera : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            MoverBola();
+    }
+
+    void MoverBola(){
+
+        //variables 
+            float posX = transform.position.x;
+            float posY = transform.position.y;
+            float desplX = Input.GetAxis("Horizontal");
+            float desplY = Input.GetAxis("Vertical");
+
+            //restringir en el eje x
+        if(posX > -10  && posX < 10 || posX < -10 && desplX > 0 || posX > 10 && desplX < 0 )
+        { transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+            //restringir en el eje y
+        if(posY > -10  && posY < 10 || posY < -10 && desplY > 0 || posY > 10 && desplY < 0 )
+        {   transform.Translate(Vector3.up * Time.deltaTime * speed * desplY);
+
+        }
+    }
+
+}
