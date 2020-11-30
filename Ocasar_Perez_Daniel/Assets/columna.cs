f7f7b4095d990f4b21512ec10283f4f170608915
DanielOcasar <daniel.ocasar.perez@gmail.com>
2020-11-30 17:55:20 +0100

    Creacion columnas
    He creado las columnas y el EmptyObject desde el cual se crean las columnas, pero tengo un error y es que las columnas no se terminan de crear aleatoriamente.

---
+++ Ocasar_Perez_Daniel/Assets/columna.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class columna : MonoBehaviour
+{
+    public GameObject Columnas;
+    public Vector3 MyPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       print(MyPos);
+    }
+}
---
+++ Ocasar_Perez_Daniel/Assets/crearcolumnas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crearcolumnas : MonoBehaviour
+{
+    public GameObject MyColumn;
+    private esfera sphere;
+    public Transform RefPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CrearColumna();
+    }
+
+    void CrearColumna (){
+        float posRandom = Random.Range(-10,10);
+
+        Vector3 DestPos = new Vector3(posRandom ,0 , 0);
+        Vector3 NewPos = RefPos.position + DestPos;
+
+        Instantiate(MyColumn, NewPos, Quaternion.identity);
+    }
+
+    IEnumerator ColumnCoroutine(){
+        for (int n=0; ;n++)
+        CrearColumna();
+        yield return new WaitForSeconds(10f);
+    }
+
+}
